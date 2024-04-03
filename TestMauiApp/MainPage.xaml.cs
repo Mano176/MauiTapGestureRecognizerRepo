211cff8eed0882f6009544978d04564ee1a90e04
Manoel Schürmeier <github@manoel-schuermeier.de>
2024-04-03 21:01:18 +0200

    init

---
+++ TestMauiApp/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+
+namespace TestMauiApp;
+
+public partial class MainPage : ContentPage, INotifyPropertyChanged
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    public new event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void NotifyPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    public bool IsFrameEnabled { get; set; }
+
+    private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+    {
+        Console.WriteLine("Here");
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        IsFrameEnabled = !IsFrameEnabled;
+        NotifyPropertyChanged(nameof(IsFrameEnabled));
+    }
+}
